cd19dec40b5f0074bf9c4ec957416b36b3c683e4
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-12-02 20:34:56 +0200

    nav

--- src/component/nav/NavBar.jsx
+++ src/component/nav/NavBar.jsx
@@ -10,10 +10,12 @@ const NavBar = () => {
   }
   return (
     <>
-      <div className="text-white bg-black flex items-center py-6 px-14 justify-between">
+      <div className="text-white bg-black flex items-center py-6 px-14 justify-between z-20">
         <div className="">
           <h1 className="font-bold text-3xl">Okunlola Muhammad</h1>
         </div>
+
+        {/* Desktop nav */}
 
         <ul className="items-center gap-6 cursor-pointer font-bold hidden md:flex">
           <NavItem>Home</NavItem>
@@ -26,7 +28,7 @@ const NavBar = () => {
             </Button>
           </li>
         </ul>
-        <div className="md:hidden text-3xl">
+        <div className="md:hidden text-3xl z-20">
           {openNavBar ? (
             <IoClose onClick={handleNavBar} />
           ) : (
@@ -34,17 +36,25 @@ const NavBar = () => {
           )}
         </div>
       </div>
-      <ul className="flex flex-col p-6 gap-6 cursor-pointer bg-black text-white font-bold md:hidden   transform transition-all duration-300 ease-out">
-        <NavItem>Home</NavItem>
-        <NavItem>Projects</NavItem>
-        <NavItem>Achievements</NavItem>
-        <NavItem>Contact</NavItem>
-        <li>
-          <Button className="transform transition duration-300 bg-white text-black hover:scale-110 hover:text-blue-600">
-            Download CV
-          </Button>
-        </li>
-      </ul>
+
+      {/* Mobile Nav */}
+      <div
+        className={`md:hidden overflow-auto transition-all duration-700 ease-in-out ${
+          openNavBar ? "h-96" : "h-0"
+        }`}
+      >
+        <ul className="flex flex-col p-6 gap-6 bg-black text-white font-bold">
+          <NavItem>Home</NavItem>
+          <NavItem>Projects</NavItem>
+          <NavItem>Achievements</NavItem>
+          <NavItem>Contact</NavItem>
+          <li>
+            <Button className="transform transition duration-300 bg-white text-black hover:scale-110 hover:text-blue-600">
+              Download CV
+            </Button>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
